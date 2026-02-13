12b76d8f4e6fdd3abc3ebfae52fe2f59e9a40c73
zygimantus <zygimantus@gmail.com>
2026-02-13 11:48:06 +0200

    WIP

--- scripts/cite-web-aina.user.js
+++ scripts/cite-web-aina.user.js
@@ -6,7 +6,7 @@
 // @match       https://www.aina.lt/*
 // @version     1.0.0
 // @author      Zygimantus
-// @icon        https://www.aina.lt/favicon.ico
+// @icon        https://zygimantus.github.io/vikipedijos-irankiai/favicon/favicon.ico
 // @run-at      document-end
 // @noframes    
 // @downloadURL https://zygimantus.github.io/vikipedijos-irankiai/scripts/cite-web-aina.user.js
@@ -145,7 +145,7 @@ generate({
     var _document$querySelect2;
     return (_document$querySelect2 = document.querySelector('meta[property="article:published_time"]')) == null ? void 0 : _document$querySelect2.getAttribute('content');
   },
-  dateFormat: raw => raw.split(' ')[0],
+  dateFormat: raw => raw.split('T')[0],
   website: 'aina.lt',
   refName: 'aina'
 });
