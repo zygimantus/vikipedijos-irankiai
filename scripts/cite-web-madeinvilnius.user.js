f10bbebace7a300901510da28caba648528fbcbc
zygimantus <zygimantus@gmail.com>
2026-02-14 16:14:29 +0200

    WIP

--- scripts/cite-web-madeinvilnius.user.js
+++ scripts/cite-web-madeinvilnius.user.js
@@ -6,7 +6,7 @@
 // @match       https://www.madeinvilnius.lt/*
 // @version     1.0.0
 // @author      Zygimantus
-// @icon        https://zygimantus.github.io/vikipedijos-irankiai/favicon/favicon.ico
+// @icon        https://madeinvilnius.lt/wp-content/uploads/2025/02/made-in-vilnius-icon-150x150.png
 // @run-at      document-end
 // @noframes    
 // @downloadURL https://zygimantus.github.io/vikipedijos-irankiai/scripts/cite-web-madeinvilnius.user.js
